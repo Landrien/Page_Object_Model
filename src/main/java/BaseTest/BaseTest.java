6a33c8096208e667cc86659ef74744f47645c0b1
Landrien <landemarrehadrien@gmail.com>
2025-03-21 16:15:14 +0100

    config.properties avec getProperty, ITestListener,screenshot, Extend report 2façon et testng

--- src/main/java/BaseTest/BaseTest.java
+++ src/main/java/BaseTest/BaseTest.java
@@ -4,6 +4,9 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.Browser;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
@@ -14,6 +17,7 @@ import utils.ConfigReader;
 import java.io.IOException;
 import java.net.URL;
 import java.time.Duration;
+import java.util.Locale;
 
 public class BaseTest {
 
@@ -24,7 +28,13 @@ public class BaseTest {
 
     @BeforeMethod
     public void setup(ITestResult result) {
-        driver = new ChromeDriver();
+        String Browser = ConfigReader.getProperty("Browser").toLowerCase();
+        switch (Browser) {
+            case "edge" -> driver = new EdgeDriver();
+            case "firefox" -> driver = new FirefoxDriver();
+            default -> driver = new ChromeDriver();
+        }
+
         driver.manage().window().maximize();
         driver.get(ConfigReader.getProperty("URL"));
         wait = new WebDriverWait(driver, Duration.ofSeconds(5));
